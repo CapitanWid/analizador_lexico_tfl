43a6f860e784b2b2a549d4d30659f1ecb486a5dc
Maurit36 <nelsonf.gallegos@uqvirtual.edu.co>
2025-05-06 02:50:11 -0500

    Ajuste de Proyecto
    Se quitan las clases que vienen del main para diferencias las ramas creadas.

--- analizadorLexicoPython/identificarSeparador.py
+++
@@ -1,8 +0,0 @@
-from token_modelo import Token, Categoria
-
-class IdentificarSeparador:
-    @staticmethod
-    def extraer(codigo_fuente, indice):
-        if codigo_fuente[indice] == ",":
-            return Token(",", Categoria.SEPARADOR, indice + 1)
-        return None
--- analizadorLexicoPython/identificarTerminal.py
+++
@@ -1,8 +0,0 @@
-from token_modelo import Token, Categoria
-
-class IdentificarTerminal:
-    @staticmethod
-    def extraer(codigo_fuente, indice):
-        if codigo_fuente[indice] == ";":
-            return Token(";", Categoria.TERMINAL, indice + 1)
-        return None
